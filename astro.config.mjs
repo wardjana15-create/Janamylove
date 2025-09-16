32087686fcbd181fde26c2bd6818d8ca30e1167e
wardjana15-create <wardjana15@gmail.com>
2025-09-16 13:18:34 +0100

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,44 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import rss from "@astrojs/rss";
+import { SITE } from "./src/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: SITE.website,
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    mdx(),
+    rss({
+      title: SITE.title,
+      description: SITE.desc,
+      site: SITE.website,
+    }),
+  ],
+  markdown: {
+    remarkPlugins: [
+      "remark-toc",
+      [
+        "remark-collapse",
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+  scopedStyleStrategy: "where",
+});
